7f5248e310ded567bbf1e99434b78b13fded1a8e
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-02-15 01:25:31 +0530

    Portfolio - React + Vite

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,15 +9,15 @@ import TypingEffect from './Typed';
 
 const HeroSection = () => {
   return (
-    <div className="h-[80%] w-full flex justify-center items-center gap-[80px] my-[64px] mx-auto pb-[8rem] overflow-x-hidden">
+    <div className="h-[80%] w-full flex justify-center items-center gap-[80px] my-[64px] mx-auto pb-[8rem] relative">
         {/* Left Part */}
       <div className='flex flex-col justify-center gap-[2rem]'>
-        <h1>Hey! Dibendu Ghosal</h1>
+        <h1 className='text-[#343d68] font-[500] text-[35px]'>Hey! Dibendu Ghosal</h1>
         <TypingEffect/>
-        <div>
+        <div className='w-[70%] mt-4 font-[500]'>
             I'm a <span>Software Engineer</span> and it is my portfolio website. Here you'll know about my journey as a software Engineer.    
         </div>
-        <button>Hire Me</button>
+        <div className='bg-[var(--bgOrange)] w-fit  text-white font-[500] px-[36px] py-3 shadowHireBtn relative transition duration-500 border-[3px] border-solid border-transparent py-[0.8rem] px-[2.3rem] btn-pink'>Hire Me</div>
       </div>
       {/* Right Part */}
       <div className="relative">
