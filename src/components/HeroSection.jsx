8d92b5eb1001dc489bec9bceb9e5d780aab57718
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-02-15 20:45:24 +0530

    Portfolio - React + Vite

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import Dots from '../assets/images/userAsset/dots.png';
 import HeroImage from '../assets/images/userAsset/plus.png';
-import myImg from '../assets/images/userAsset/_myPhoto.jpg';
+import myImg from '../assets/images/userAsset/_myImg.jpg';
 import blobVector from '../assets/images/userAsset/blobVector.png';
 import zigzagImg from '../assets/images/userAsset/zigzags.png';
 import circleImg from '../assets/images/userAsset/circle.png';
@@ -20,14 +20,13 @@ const HeroSection = () => {
         <div className='bg-[var(--bgOrange)] w-fit  text-white font-[500] px-[36px] py-3 shadowHireBtn relative transition duration-500 border-[3px] border-solid border-transparent py-[0.8rem] px-[2.3rem] btn-pink'>Hire Me</div>
       </div>
       {/* Right Part */}
-      <div className="relative">
-        <img src={Dots} alt="" />
-        <img src={HeroImage} alt="" />
-        <img src={myImg} alt="" />
-        <img src={blobVector} alt="" />
-        <img src={zigzagImg} alt="" />
-        <img src={circleImg} alt="" />
-        <img src={cubeImg} alt="" />
+      <div className="relative ">
+        <img src={Dots} alt="" className='absolute -bottom-4 -right-4 animate-bounce z-10'  />
+        <img src={HeroImage} alt="" className='absolute z-10 -top-[0.8rem] left-[50%] blobVct' />
+        <img src={myImg} alt="" className='w-[355px] h-[384px] object-cover object-top p-[2.5rem]  transition-all imgAnimation -z-[9]' />
+        <img src={zigzagImg} alt="" className='absolute z-10 top-[1.5em] -left-[0.3em] zigzagAnm' />
+        <img src={circleImg} alt=""  className='absolute circleAnm bottom-0 left-0 z-10'/>
+        <img src={cubeImg} alt="" className='absolute cubeAnm top-0 right-0 z-10' />
       </div>
       {/* Faded Text */}
       <div className='absolute select-none text-[7em] text-[rgb(231,231,231)] font-bold -bottom-[16%] left-[5%]'>Dibendu Ghosal</div>
